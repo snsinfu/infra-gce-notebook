c98974835e3cca083fc85ddeb5bd6d6d4fda1d6d
snsinfu <snsinfu@gmail.com>
2018-12-16 01:54:39 +0900

    Add machine instance terraform

---
+++ machine/vars.tf
@@ -0,0 +1,36 @@
+variable "google_project" {
+}
+
+variable "google_zone" {
+}
+
+variable "machine_type" {
+}
+
+variable "machine_image" {
+}
+
+variable "accelerator_type" {
+}
+
+variable "disk_type" {
+}
+
+variable "disk_size" {
+}
+
+variable "storage_bucket" {
+}
+
+variable "storage_class" {
+}
+
+variable "storage_location" {
+}
+
+variable "ssh_user" {
+}
+
+variable "ssh_pubkeys" {
+  type = "list"
+}
