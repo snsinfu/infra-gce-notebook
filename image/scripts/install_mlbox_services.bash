6781410c5fac4e7a20ab83292ddc355a15b6dd89
snsinfu <snsinfu@gmail.com>
2018-12-16 03:55:46 +0900

    image: Add jupyterlab appdir links

--- image/scripts/install_mlbox_services.bash
+++ image/scripts/install_mlbox_services.bash
@@ -25,5 +25,9 @@ install -m 0755 /tmp/assets/mlbox-jupyter /etc/skel/bin
 install -m 0644 /tmp/assets/mlbox-jupyter.service /etc/skel/.config/systemd/user
 
 install -m 0755 -d /etc/skel/.jupyter
-install -m 0755 -d /etc/skel/.jupyter/app
 install -m 0644 /tmp/assets/jupyter_notebook_config.py /etc/skel/.jupyter
+
+install -m 0755 -d /etc/skel/.jupyter/app
+ln -s /opt/miniconda3/envs/mlbox/share/jupyter/lab/schemas /etc/skel/.jupyter/app
+ln -s /opt/miniconda3/envs/mlbox/share/jupyter/lab/static /etc/skel/.jupyter/app
+ln -s /opt/miniconda3/envs/mlbox/share/jupyter/lab/themes /etc/skel/.jupyter/app
