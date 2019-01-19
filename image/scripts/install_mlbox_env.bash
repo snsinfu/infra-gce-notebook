30e01e66c3b48f447850102b1a7390929466df95
snsinfu <snsinfu@gmail.com>
2019-01-20 00:55:42 +0900

    Add conda packages

--- image/scripts/install_mlbox_env.bash
+++ image/scripts/install_mlbox_env.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -eux -o pipefail
 
-/opt/miniconda3/bin/conda create \
-    --name mlbox                 \
-    --file /tmp/assets/mlbox-env.spec
+/opt/miniconda3/bin/conda env create \
+    --file /tmp/assets/mlbox-env.yml
